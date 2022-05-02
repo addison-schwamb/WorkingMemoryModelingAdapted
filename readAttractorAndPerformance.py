4203bd0a6fdce3999eee4318230c65303682cac5
addisonschwamb <addisonleighs@gmail.com>
2022-05-02 10:55:52 -0500

    Add small function to read the attractor and performance stats from a saved network file

---
+++ readAttractorAndPerformance.py
@@ -0,0 +1,10 @@
+from train_force import *
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', type=str)
+args = parser.parse_args()
+print(args.d)
+dmg_params, dmg_x = read_data_variable_size(args.d+'_500_training_steps_1.0%_removed',prefix='train',dir='')
+print('Attractor: ', dmg_params['attractor'])
+print('Performance: ', dmg_params['pct_correct'])
